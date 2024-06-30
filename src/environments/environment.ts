5ad7c3621c0883c84d429203840b5d4fa7c2ccc0
Dubravka Drčelić <duca.drcelic25@gmail.com>
2024-06-30 21:08:52 +0200

    env file and environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,19 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+require('dotenv').config();
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: env.,
+    authDomain:,
+    databaseURL:,
+    projectId:,
+    storageBucket:,
+    messagingSenderId:,
+    appId:
+  }
 };
 
 /*
